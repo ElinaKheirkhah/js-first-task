381dcf429f61a2c21464515fc9bc55216e1e5c3e
elinakheirkhah <elinakhairkhah@yahoo.com>
2025-01-31 01:23:06 +0330

    one-final

--- app.js
+++ app.js
@@ -1,16 +1,19 @@
 //Task one: Create a function that checks if a number is prime
 
-let n = 12;
+let n = 13;
 let isPrime = true;
 
-if (n <= 1) {
-  isPrime = false;
-} else {
-    for (let i = 2; i < n; i++) {
-        if (n % i === 0) {
-            isPrime = false;
-            break;
+function primeNumber(n, isPrime) {
+    if (n <= 1) {
+        isPrime = false;
+    } else {
+        for (let i = 2; i < n; i++) {
+            if (n % i === 0) {
+                isPrime = false;
+                break;
+            }
         }
     }
 }
+
 console.log({n}, isPrime ? "it's a prime number" : "it's not a prime number");
